3c63f448f7103dceb1e2de1087f10e5fba97594f
Ali Azimo <webdesign.aejl@gmail.com>
2024-09-10 16:46:09 -0300

    Conectar ao MongoDB

--- Api/index.js
+++ Api/index.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+
+//Conectar com banco de dados online
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('Conectado ao Banco de dados Mongo!');
+    }).catch((err) => {
+        console.log(err)
+    });
 const app = express();
+
 
 
 
 //Porta de entra
 app.listen(5000, () => {
-    console.log('Servidor esta aberto no portal 5000');
+    console.log('Servidor esta aberto no portal 5000!');
 });
